63c8f3697d70465adbd375fcbde4b9c76a7ac622
Cheonhwa Kim <trigonotis97@gmail.com>
2021-08-10 23:05:22 +0900

    solved boj1012 10844 12865 1676

--- src/baekjoon/BOJ_12865.java
+++ src/baekjoon/BOJ_12865.java
@@ -4,7 +4,9 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+
+import static java.lang.Integer.max;
 
 
 public class BOJ_12865 {
@@ -14,16 +16,34 @@ public class BOJ_12865 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int result;
+        int max_num=100001;
         StringTokenizer st=new StringTokenizer(br.readLine());
         int h=Integer.parseInt(st.nextToken());
         int max_weight=Integer.parseInt(st.nextToken());
-        int [][] arr = new int[100001][100001];
-        for (int i = 0; i < h; i++) {
+        int [][] stuff=new int [h+1][2];
 
+        int [][] valMem = new int[h+1][max_weight+1];
+        for (int i = 1; i <= h; i++) { //입력
+            st=new StringTokenizer(br.readLine());
+            stuff[i][0]= Integer.parseInt(st.nextToken()); //w
+            stuff[i][1]= Integer.parseInt(st.nextToken()); //v
+        }
+        //dp
+        for (int i = 0; i <=h ; i++) {
+            for (int w = 0; w <=max_weight ; w++) {
+                if(i==0||w==0){
+                    valMem[i][w]=0;
+
+                }
+                else if(w<stuff[i][0])
+                    valMem[i][w]=valMem[i-1][w];
+                else
+                    valMem[i][w]=max(valMem[i-1][w],valMem[i-1][w-stuff[i][0]]+stuff[i][1]);
+
+            }
         }
 
-
-        System.out.println(result);
+        System.out.println(valMem[h][max_weight]);
 
     }
 }
---
+++ src/leetcode/problem2.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class problem2 {
+}
+
+
+
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res=new ListNode();
+        for (int i = 0; i < ; i++) {
+            
+        }
+        return res;
+    }
+}
