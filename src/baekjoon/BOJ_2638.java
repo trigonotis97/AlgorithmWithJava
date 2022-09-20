6790dd709656095fe39e85ebb1c8ac5e786a4a85
Cheon hwa Kim <trigonotis97@gmail.com>
2022-09-20 22:31:03 +0900

    doing boj_2638, start algorithm with javascript

---
+++ src/baekjoon/BOJ_2638.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+//doing
+//https://www.acmicpc.net/problem/2638
+public class BOJ_2638 {
+    public static void main(String[] args) throws IOException {
+
+
+        //input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n, m;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n + 1][m + 1];
+        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j]==0) arr[i][j] = -1;
+                else arr[i][j] = 0;
+            }
+        }
+
+        Queue<Point> queue = new PriorityQueue<>();
+
+
+    }
+
+    static class Point{
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
