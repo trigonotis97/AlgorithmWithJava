60fae14a7a96d3a6eea4de5feebe74ee2688aafd
cheonhwa kim <trigonotis97@gmail.com>
2023-04-04 09:28:36 +0900

    solved boj 9372

---
+++ src/baekjoon/BOJ_9372.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ_9372 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m ; j++) {
+                br.readLine();
+            }
+            list.add(n-1);
+        }
+        list.stream().forEach(System.out::println);
+    }
+}
