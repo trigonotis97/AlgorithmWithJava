233ebb582aa83528a944557428b53c8a97875a1e
cheonhwa kim <trigonotis97@gmail.com>
2023-04-06 17:03:21 +0900

    solve boj 11660

--- src/baekjoon/BOJ_11660.java
+++ src/baekjoon/BOJ_11660.java
@@ -3,10 +3,11 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 
-//doing
 public class BOJ_11660 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,18 +23,30 @@ public class BOJ_11660 {
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 if (i != 0) arr[i][j] += arr[i - 1][j];
                 if (j != 0) arr[i][j] += arr[i][j - 1];
-                if (i != 0 && j != 0) arr[i][j] -= arr[i - 1][j-1];
+                if (i != 0 && j != 0) arr[i][j] -= arr[i - 1][j - 1];
 
             }
         }
         int x1, y1, x2, y2;
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            x1 = Integer.parseInt(st.nextToken());
-            y1 = Integer.parseInt(st.nextToken());
-            x2 = Integer.parseInt(st.nextToken());
-            y2 = Integer.parseInt(st.nextToken());
-            System.out.println(arr[y2 - 1][x2 - 1] - arr[y1 - 1][x1 - 1]);
+            y1 = Integer.parseInt(st.nextToken()) - 1;
+            x1 = Integer.parseInt(st.nextToken()) - 1;
+            y2 = Integer.parseInt(st.nextToken()) - 1;
+            x2 = Integer.parseInt(st.nextToken()) - 1;
+            int sum = arr[y2][x2];
+            if (y1 > 0) {
+                sum -= arr[y1 - 1][x2];
+            }
+            if (x1 > 0) {
+                sum -= arr[y2][x1 - 1];
+            }
+            if (x1 > 0 && y1 > 0) {
+                sum += arr[y1 - 1][x1 - 1];
+            }
+            list.add(sum);
         }
+        list.forEach(System.out::println);
     }
 }
