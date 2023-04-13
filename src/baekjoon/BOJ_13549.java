ba702c0df8b9c63a33447d87b00f9d0f54ad6151
cheonhwa kim <trigonotis97@gmail.com>
2023-04-13 17:52:08 +0900

    solve boj 13549

--- src/baekjoon/BOJ_13549.java
+++ src/baekjoon/BOJ_13549.java
@@ -3,9 +3,8 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 //doing
 public class BOJ_13549 {
     public static void main(String[] args) throws IOException {
@@ -14,15 +13,38 @@ public class BOJ_13549 {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int curPos = n;
+        int pos = n;
 
-        int [] arr = new int[100001];
-        arr[curPos] = 0;
-        Queue<Integer> queue = new PriorityQueue<>();
-        queue.add(curPos);
-        while (!queue.isEmpty()){
+        int[] arr = new int[222223];
 
+        for (int i = 0; i < 222223; i++) {
+            arr[i] = Integer.MAX_VALUE;
         }
+        arr[pos] = 0;
+        Deque<Integer> dq = new LinkedList<>();
+        dq.addFirst(pos);
 
+        while (!dq.isEmpty()) {
+            pos = dq.pollFirst();
+            System.out.println(pos);
+            if (pos == k) {
+                System.out.println(arr[pos]);
+                return;
+            }
+            int db = pos * 2;
+            if (db < 200000 && arr[pos] < arr[db]) {
+                arr[db] = arr[pos];
+                dq.addFirst(db);
+            }
+            int l = pos - 1, r = pos + 1;
+            if (l >=0 && l < 200000 && arr[pos] + 1 < arr[l]) {
+                arr[l] = arr[pos] + 1;
+                dq.addLast(l);
+            }
+            if (r < 200000 && arr[pos] + 1 < arr[r]) {
+                arr[r] = arr[pos] + 1;
+                dq.addLast(r);
+            }
+        }
     }
 }
