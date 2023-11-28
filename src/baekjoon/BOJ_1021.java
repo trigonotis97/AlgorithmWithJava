f7b0355f5af998798eea5ca0d51f900bbe427ba3
cheonhwa kim <trigonotis97@gmail.com>
2023-11-28 17:05:58 +0900

    solve boj 1021

--- src/baekjoon/BOJ_1021.java
+++ src/baekjoon/BOJ_1021.java
@@ -3,47 +3,43 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1021 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n= Integer.parseInt(st.nextToken()); //큐의 크기 (이 수만큼 꽉참)
-        int m= Integer.parseInt(st.nextToken()); //뽑아내야하는 원소 개수
+        int n = Integer.parseInt(st.nextToken()); //큐의 크기 (이 수만큼 꽉참)
+        int m = Integer.parseInt(st.nextToken()); //뽑아내야하는 원소 개수
 
         //1: poll
         //2: pollfirst addLast
         //3 : polllast add first
-        st= new StringTokenizer(br.readLine());
-        int offset=0,count=0;
+        st = new StringTokenizer(br.readLine());
+        int count = 0;
+        LinkedList<Integer> deque = new LinkedList<>();
+        for (int i = 1; i < n+1 ; i++) {
+            deque.addLast(i);
+        }
+
         for (int i = 0; i < m; i++) {
-            int x=Integer.parseInt(st.nextToken());
-            //deque.addLast(x);\
-            x=x+offset<0 ? x+offset+n+1 : x+offset;
-
-            if(x>n)
-                x-=n;
-            else if(x<=-n)
-                x+=n;
-            if(x>n/2){ //오른쪽으로 이동
-                count+= n-x+1;
-                offset+=count;
+            int num = Integer.parseInt(st.nextToken());
+            int dir; //1 이면 first 2 면 last
+            if(deque.indexOf(num) > deque.size()/2)
+                dir = 2;
+            else
+                dir = 1;
+            while (deque.get(0) != num){
+                if(dir == 1) {
+                    deque.addLast(deque.pollFirst());
+                }
+                else {
+                    deque.addFirst(deque.pollLast());
+                }
+                count++;
             }
-            else{//왼똑으로 이동
-                count+=x-1;
-                offset-=count;
-            }
-            n--;
-            offset--; //항상 앞에서 뽑아주기때문
-            if(offset>=n)
-                offset-=n;
-            else if(offset<=-n)
-                offset+=n;
-
+            deque.pollFirst();
         }
         System.out.println(count);
     }
