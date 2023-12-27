720a427c382a144de807873c9cd3e5b644a5f4c8
cheonhwa kim <trigonotis97@gmail.com>
2023-12-27 17:06:23 +0900

    doing boj

---
+++ src/baekjoon/BOJ_1446.java
@@ -0,0 +1,50 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1446 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int curPos = 0, curLen = 0;
+        Road[] arr = new Road[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+            arr[i] = new Road(s, e, l);
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            if (curPos >= arr[i].start && arr[i].end - curPos > arr[i].length) {
+                curPos=arr[i].end;
+                curLen += arr[i].length
+            } else if(curPos<arr[i].start && Qatt[curPosp ]) {
+            }
+        }
+    }
+
+    static class Road implements Comparable<Road> {
+        int start;
+        int end;
+        int length;
+
+        Road(int s, int e, int l) {
+            start = s;
+            end = e;
+            length = l;
+        }
+
+        @Override
+        public int compareTo(Road o) {
+            return o.start - this.start;
+        }
+    }
+}
---
+++ src/baekjoon/BOJ_4485.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//doing
+public class BOJ_4485 {
+    static final int INF = 200000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+        StringTokenizer st;
+        while (true){
+            n = Integer.parseInt(br.readLine());
+            if(n==0) break;
+
+            int [][] arr = new int[n][n];
+            int [][] warr = new int[n][n];
+            Arrays.fill(warr,INF);
+            for (int i = 0; i < n; i++) {
+                st=new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    arr[i][j]=Integer.parseInt(st.nextToken());
+                }
+            }
+            //logic
+            int count = 0;
+
+        }
+
+
+    }
+    class Point {
+        int x; int y;
+        Point(int y, int x){
+           this.x = x;
+           this.y = y;
+        }
+    }
+}
