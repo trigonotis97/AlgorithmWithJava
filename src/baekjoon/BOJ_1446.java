a89c319da2f97c4c04c84f6296dd2e331973a01d
Cheon hwa Kim <trigonotis97@gmail.com>
2024-01-31 22:09:11 +0900

    solve boj 1446

--- src/baekjoon/BOJ_1446.java
+++ src/baekjoon/BOJ_1446.java
@@ -3,37 +3,54 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1446 {
+    private static int INF = 10001;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
-        int curPos = 0, curLen = 0;
-        Road[] arr = new Road[n];
+        List<Road> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int l = Integer.parseInt(st.nextToken());
-            arr[i] = new Road(s, e, l);
+            if(e>d)continue;
+            if(e-s<=l)continue;
+            list.add(new Road(s, e, l)) ;
+
         }
-        Arrays.sort(arr);
-        /*
-        for (int i = 0; i < n; i++) {
-            if (curPos >= arr[i].start && arr[i].end - curPos > arr[i].length) {
-                curPos=arr[i].end;
-                curLen += arr[i].length
-            } else if(curPos<arr[i].start && Qatt[curPosp ]) {
+        Collections.sort(list, new Comparator<Road>() {
+            @Override
+            public int compare(Road o1, Road o2) {
+                return o1.start - o2.start;
             }
+        });
+        int curPos = 0;
+        int[] dist = new int[INF];
+        Arrays.fill(dist, INF);
+        dist[0] = 0;
+        int ind = 0;
+        while (curPos <= d) {
+            if (ind < list.size() && curPos == list.get(ind).start) {
+                Road r = list.get(ind);
+                dist[r.end] = Math.min(dist[r.end], dist[curPos] + r.length);
+                ind ++;
+            } else {
+                dist[curPos + 1] = Math.min(dist[curPos + 1], dist[curPos] + +1);
+                curPos++;
+            }
+
         }
-         */
+        System.out.println(dist[d]);
+
     }
 
-    static class Road implements Comparable<Road> {
+    static class Road {
         int start;
         int end;
         int length;
@@ -42,11 +59,6 @@ public class BOJ_1446 {
             start = s;
             end = e;
             length = l;
-        }
-
-        @Override
-        public int compareTo(Road o) {
-            return o.start - this.start;
         }
     }
 }
